29b06e26090645617a729947f4d5a9bec5135a0f
ChickenHawkXP <47929127+ChickenHawkXP@users.noreply.github.com>
2019-08-29 15:03:34 -0400

    Added first function

--- Payroll Management System/Source.cpp
+++ Payroll Management System/Source.cpp
@@ -13,15 +13,48 @@ Functionality:
 
  Project Source Idea: https://www.codewithc.com/payroll-management-system-project-in-c/
 */
+class Employee
+{
+public:
+	std::string name, lname, address, title, joinDate,serialNumber, phoneNumber;
+	char grade;
+	int age, salary;
+
+	void addEmployee() {
+		std::ofstream employeeData;
+		employeeData.open("employeeData.txt");
+		std::cout << "\nEnter the employees first name: ";
+		std::cin >> name;
+		std::cout << "\nEnter the employees last name: ";
+		std::cin >> lname;
+		std::cout << "\nEnter the employees code number: ";
+		std::cin >> serialNumber;
+		std::cout << "\nEnter the employees address: ";
+		std::cin >> address;
+		std::cout << "\nEnter the employees phone number: ";
+		std::cin >> phoneNumber;
+		std::cout << "\nEnter the employees title:";
+		std::cin >> title;
+		std::cout << "\nEnter the employees age: ";
+		std::cin >> age;
+		std::cout << "\nEnter the employees grade: ";
+		std::cin >> grade;
+		std::cout << "\nEnter the employees salary: ";
+		std::cin >> salary;
+		employeeData << name << "," << lname << "," << serialNumber << "," << address << "," << phoneNumber << "," << title << "," << age << "," << grade << "," << salary;
+		std::cout << "Employee added!";
+	}
+};
 void displayMenu() {
 	std::cout << "What would you like to do?\n [1] Add Employee\n [2] Modify Employee\n [3] Delete Employee\n"
 			  << " [4] Print Employee Salary Slip\n [5] Dispay an Employee Record\n [6] List Employees\n [7] Exit\nMake your selection: ";
-	
 }
+
 int main()
 {
 	int menuChoice;
 	std::ofstream employeeData;
+	Employee employee;
 	employeeData.open("employeeData.txt");
 	while (employeeData.is_open())
 	{
@@ -29,6 +62,7 @@ int main()
 		std::cin >> menuChoice;
 		switch (menuChoice){
 		case 1:
+			employee.addEmployee();
 			break;
 		case 2:
 			break;
